f9c2ae9dc14f00bdad31c72f2e16bf53eea2e0d4
Agustín Kenny <aguskenny24@gmail.com>
2025-02-03 12:39:36 -0300

    - a cara de dog

--- app/page.tsx
+++ app/page.tsx
@@ -11,68 +11,26 @@ import Reality from "@/components/sections/Reality";
 import Program from "@/components/sections/Autocuidarse";
 import StudentResults from "@/components/sections/StudentResults";
 import Module from "@/components/sections/ModuleHeader";
-import { getLanding } from "@/lib/contentful";
-import { FC } from "react";
 
-export const revalidate = 60;
-
-export default async function Home() {
-  const getLandingData = await getLanding();
-  const components =
-    getLandingData.pageCollection.items[0].componentsCollection.items;
-
-  const componentMap: Record<
-    string,
-    FC<any> // Esto significa que cada componente es un Functional Component y puede aceptar cualquier prop
-  > = {
-    Header,
-    Hero,
-    AboutPilar,
-    Testimonials,
-    Pricing,
-    Faq,
-    CallToAction,
-    CourseIncludes,
-    VideoTestimonial,
-    Reality,
-    Program,
-    StudentResults,
-    Module,
-  };
-
-  // Encuentra la posición del primer `Module`
-  const firstModuleIndex = components.findIndex(
-    (component) => component.__typename === "Module"
-  );
-
+export default function Home() {
   return (
     <main className="min-h-screen bg-[#f6f7f4] pb-8">
-      {/* 
-      
+      <Header  />
+      <Hero />
+      <Reality />
+      <Testimonials />
+      <Program />
+      <VideoTestimonial />
       <p className="font-bold text-[1rem] sm:text-[1.5rem] leading-tight text-[#444] text-l py-4 text-center max-w-2xl mx-auto">
         Lo que aprenderás en el curso…
       </p>
-     
-      */}
-      {components.map((component, index) => {
-        const Component = componentMap[component.__typename]; // Mapea el tipo al componente
-        if (!Component) return null; // Si el componente no existe en el mapa, no se renderiza
-
-        // Renderiza el componente con sus props
-        return (
-          <>
-            {index === firstModuleIndex && (
-              <p className="font-bold text-[1rem] sm:text-[1.5rem] leading-tight text-[#444] text-l py-4 text-center max-w-2xl mx-auto">
-                Lo que aprenderás en el curso…
-              </p>
-            )}
-            <Component
-              key={index}
-              {...component} // Asegúrate de que `props` contiene los datos dinámicos necesarios
-            />
-          </>
-        );
-      })}
+      <Module />
+      <CourseIncludes />
+      <AboutPilar />
+      <StudentResults />
+      <Pricing />
+      <Faq />
+      <CallToAction />
     </main>
   );
 }
